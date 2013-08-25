7e70ca1484a1628ba33cbc94ecbf99328bb61fa0
Haoliang Quan <haoliang.quan@gmail.com>
2013-08-25 18:10:39 -0400

    big bug fixed

--- mediastreamer_CMU.py
+++ mediastreamer_CMU.py
@@ -13,7 +13,7 @@ def cal_sid_port(name):
 	for i in range(0, len(name)):
 		port += (ord(name[i]) - 10) * (10 ** i)
 
-	while (port > 65535):
+	while (port > 60000):
 		port /= 2
 
 	hex_port = str(hex(port))[2:]
@@ -52,8 +52,8 @@ try:
 	(remote_ad, remote_hid) = getAD_HID(remote_dag)
 	remote_port = cal_sid_port(remote_name)[1]
 
-	audio_command = "gnome-terminal -x mediastream --local %d --remote %s\\ %s:%d --payload 110" % (local_port, remote_ad, remote_hid, remote_port)
-	video_command = "gnome-terminal -x mediastream --local %d --remote %s\\ %s:%d --payload 102 --mtu 1450" % (local_port + 1, remote_ad, remote_hid, remote_port + 1)
+	audio_command = "gnome-terminal -x mediastream --local %d --remote %s\\ %s:%d --payload 110" % (local_port + 16, remote_ad, remote_hid, remote_port + 16)
+	video_command = "gnome-terminal -x mediastream --local %d --remote %s\\ %s:%d --payload 102 --mtu 1450" % (local_port, remote_ad, remote_hid, remote_port)
 
 	os.system(video_command)
 	time.sleep(1)
